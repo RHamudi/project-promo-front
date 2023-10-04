61b63b1c9987f03f2bdb6e5768e4a5d83c9f78c0
RHamudi <94305379+RHamudi@users.noreply.github.com>
2023-10-03 23:06:20 -0300

    feat: model categorias

---
+++ src/components/Categorias/index.js
@@ -0,0 +1,72 @@
+import { 
+    Text,
+    View,
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+} from "react-native";
+import { FontAwesome5 } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
+
+
+export default function Categorias()
+{
+    return(
+        <View style={{
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: 150,
+            backgroundColor: '#5456'
+        }}>
+            <SafeAreaView style={styles.container}>
+                <ScrollView horizontal={true} style={styles.scrollView}>
+                    <View style={styles.containerIcons}>
+                        <Text style={{paddingBottom: 15, fontSize: 20}}>Categorias</Text>
+                        <View style={styles.viewIcons}>
+                            <View style={styles.icons}>
+                                <FontAwesome5 name="tshirt" size={35} color="black" />
+                            </View>
+                            <View style={styles.icons}>
+                                <Ionicons name="fast-food" size={35} color="black" />
+                            </View>
+                            <View style={styles.icons}>
+                                <MaterialIcons name="computer" size={35} color="black" />
+                            </View>
+                        </View>
+                    </View>
+                </ScrollView>
+            </SafeAreaView> 
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: StatusBar.currentHeight,
+    },
+    scrollView: {
+        
+    },
+    containerIcons: {
+        alignItems:'center',
+        justifyContent: 'center'
+    },
+    text: {
+        fontSize: 42,
+    },
+    viewIcons: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 10
+    },
+    icons: {
+        backgroundColor: 'rgba(124, 131, 133, 0.8)',
+        flexDirection: 'row',
+        padding: 4,
+        borderRadius: 5
+    }
+  });
