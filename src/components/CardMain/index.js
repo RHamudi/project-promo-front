9b7ce96e749e4bb37bb1ff135b3e97120ff59d43
RHamudi <94305379+RHamudi@users.noreply.github.com>
2023-10-03 23:06:08 -0300

    feat: create card main

---
+++ src/components/CardMain/index.js
@@ -0,0 +1,62 @@
+import {React, useState, useEffect} from 'react';
+import {Text, View, StyleSheet, ScrollView, Image} from 'react-native';
+import { axiosApi } from '../../Services/http-client';
+
+const CardMain = ({item}) => {
+    const [req, setReq] = useState([]);
+
+    useEffect(()=> {
+        axiosApi.get(`http://192.168.1.64:5293/api/product/getbyid?idEmpresa=${item.idEmpresa}`).then((response)=> {     
+            setReq(response.data.data)
+        }).catch((err) => console.log(err))
+    }, [])
+
+    console.log(item.contatos.number)
+    return (
+        <View style={styles.container}>
+            <ScrollView style={styles.scroll} horizontal={true}>
+                <View style={styles.itens}>
+                    {req.map((product, index)=> (
+                        <View key={product.idEmpresa} style={styles.containerProduct}>
+                            <View style={styles.cardProduct}>
+                                <Image
+                                style={styles.cardProduct}
+                                src={product.imagens}
+                                />
+                            </View>
+                            <Text style={styles.text}>{product.nome}</Text>
+                        </View>
+                    ))}
+                </View>
+            </ScrollView>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        height: 170,
+    },
+    text: {
+        textAlign: 'center'
+    },
+    scroll: {
+        margin: 15,
+    },
+    itens: {
+        flexDirection: 'row',
+        gap: 10
+    },
+    containerProduct: {
+        
+      },
+    cardProduct: {
+        backgroundColor: 'white',
+        height: 120,
+        width: 120,
+        borderRadius: 6
+    }
+
+})
+
+export default CardMain;
