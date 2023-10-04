c1d1724ded2ad5c62d0e180a3c24144d688fca9c
RHamudi <94305379+RHamudi@users.noreply.github.com>
2023-10-03 23:09:36 -0300

    feat: create page home

---
+++ src/Pages/Home/index.js
@@ -0,0 +1,34 @@
+import { Button, StyleSheet, View, TouchableHighlight } from 'react-native';
+import Categorias from '../../components/Categorias/index';
+import Business from '../../components/Business/Index';
+import { axiosApi } from '../../Services/http-client'
+import { useState, useEffect } from 'react';
+
+export default function Home({navigation}) {
+  const [req, setReq] = useState([]);
+
+  useEffect(()=> {
+      axiosApi.get('http://192.168.1.64:5293/api/business/getall').then((response)=> {     
+          setReq(response.data.data)
+      }).catch((err) => console.log(err))
+  }, [])
+
+  return (
+      <View style={styles.container}>
+        <Categorias/>
+        {req.map((item, index) => (
+          <TouchableHighlight key={index} onPress={()=> navigation.navigate('Business', {item: item})}>
+            <Business  item={item}/>
+          </TouchableHighlight>
+        ))}
+      </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#003366',
+  },
+  cards: {
+  }
+});
