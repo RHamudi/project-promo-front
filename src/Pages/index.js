3780b738a40520047fcf3ffebb4fcb40f68508fa
RHamudi <94305379+RHamudi@users.noreply.github.com>
2023-10-25 15:37:46 -0300

    feat: adding page login

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,14 +1,17 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from './Home';
 import Business from './Business';
+import Login from './Login/Index';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function MyStack() {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="Business" component={Business}/>
-    </Stack.Navigator>
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Business" component={Business}/>
+      <Tab.Screen name="Login" component={Login}/>
+    </Tab.Navigator>
   );
 }
