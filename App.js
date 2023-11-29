ed1ef7d21a48e284f45e788a2150543cfd04c3e2
RHamudi <94305379+RHamudi@users.noreply.github.com>
2023-11-29 13:00:50 -0300

    imports

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import { NavigationContainer } from '@react-navigation/native';
-import MyStack from './src/Pages';
+import MyStack from './src/Pages/Screens';
 import { Provider } from 'react-redux';
 import store from './src/Redux/store';
-
+import { CompanyStackScreen } from './src/Pages/Screens';
 
 export default function App() {
 
