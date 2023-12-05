a7d5bca1f6e19eea6c564a8c071cab231d6f9e25
RHamudi <94305379+RHamudi@users.noreply.github.com>
2023-12-05 01:54:37 -0300

    feat: adding states in page

--- src/Pages/CreateBusiness/index.js
+++ src/Pages/CreateBusiness/index.js
@@ -1,6 +1,18 @@
+import { useState } from "react";
 import { Text, View } from "react-native";
 
 export default function AddBusiness(){
+    const [Name, setName] = useState();
+    const [Description, setDescription] = useState();
+    const [Logo, setLogo] = useState();
+    const [Location, setLocation] = useState();
+    const [Email, setEmail] = useState();
+    const [Number, setNumber] = useState();
+    const [Site, setSite] = useState();
+    const [Category, setCategory] = useState();
+    const [Operation, setOperation] = useState();
+    const [GeoData, setGeoData] = useState();
+
     return (
         <View>
             <Text>
