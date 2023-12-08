221d6b6eef4b0fc313cc3b1175c22d7842e61d63
RHamudi <94305379+RHamudi@users.noreply.github.com>
2023-12-08 10:29:20 -0300

    update src/Pages/Account/index.js

--- src/Pages/Account/index.js
+++ src/Pages/Account/index.js
@@ -10,7 +10,7 @@ export default function Account({navigation}){
         <View>
             <Text>Usuario: {User.nome}</Text>
             <Button 
-            title="Criar Empresa"
+            title="Criar Empresa testando auto commit"
             onPress={()=> navigation.navigate("AddBusiness")}
             />
         </View>
