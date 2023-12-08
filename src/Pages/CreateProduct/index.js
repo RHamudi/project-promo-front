829a7a536266cf5322b84c5de9da38ce0078e218
RHamudi <94305379+RHamudi@users.noreply.github.com>
2023-12-08 14:59:30 -0300

    Add form inputs for creating a product
    Added form inputs for Name, Description, Images, and Price in the CreateProduct component.

--- src/Pages/CreateProduct/index.js
+++ src/Pages/CreateProduct/index.js
@@ -1,9 +1,29 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { Text, TextInput, View } from "react-native";
 
 export default function CreateProduct(){
+    const [Name, setName] = useState();
+    const [Description, setDescription] = useState();
+    const [Images, setImages] = useState();
+    const [Price, setPrice] = useState();
+
+
     return (
         <View >
             <Text>teste final arquivo</Text>
+            <TextInput 
+            placeholder="Name"
+            />
+            <TextInput 
+            placeholder="Description"
+            />
+            <TextInput 
+            placeholder="Images"
+            />
+            <TextInput 
+            placeholder="Price"
+            />
+
         </View>
     )
 }
