00eafdcc5335be82dd70b42b5b34736627d99f10
RHamudi <94305379+RHamudi@users.noreply.github.com>
2023-12-26 09:57:53 -0300

    Refactor login page
    Refactored the login page component to improve code readability and organization. Made changes to the layout and styling of the input fields and submit button.

--- src/Pages/Login/Index.js
+++ src/Pages/Login/Index.js
@@ -53,7 +53,7 @@ const Login = ({navigation}) => {
 
     return (
         <View className="flex-1 justify-center items-center gap-y-2">
-            <View className="grid gap-6 mb-6">
+            <View className="justify-center">
                 <Controller 
                     control={control}
                     rules={{
@@ -88,22 +88,25 @@ const Login = ({navigation}) => {
                     }}
                     render={({
                         field: {onChange, onBlur, value}}) => (
+                            <>
+                            <Text className="pt-3 block mb-2 text-sm font-medium text-gray-900 dark:text-white">Senha</Text>
                             <TextInput 
+                            className="py-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 placeholder='Digite sua senha'
                                 onBlur={onBlur}
                                 onChangeText={onChange}
                                 value={value}
                             />
+                            </>
                         )}
                     name='Password'
                 />
                 {errors.Password && <Text>{errors.Password.message}</Text>}
             </View>
-
-            <Button 
-                title='submit'
-                onPress={handleSubmit(onSubmit)}
-            />
+            
+            <TouchableOpacity onPress={handleSubmit(onSubmit)}>
+                <Text className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</Text>
+            </TouchableOpacity>
             
             <Text>Não possui uma conta? </Text>
             <TouchableOpacity style={styles.button} onPress={onPress}>
