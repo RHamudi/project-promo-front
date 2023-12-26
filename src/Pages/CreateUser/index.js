5f09fd81a3791bda596e9a19c463347d0482a595
RHamudi <94305379+RHamudi@users.noreply.github.com>
2023-12-26 10:44:35 -0300

    Refactor CreateUser component
    Refactored the CreateUser component to use tailwind CSS classes for styling. Added classNames to the Text and TextInput components for consistent styling. Updated the TextInput component to use a larger width and padding. Added a View component to wrap the Text and TextInput components for better organization. Updated the Button component to have a title of 'Criar'.

--- src/Pages/CreateUser/index.js
+++ src/Pages/CreateUser/index.js
@@ -39,69 +39,75 @@ export default function CreateUser({navigation}){
     }
 
     return(
-        <View>
+        <View className="flex-1 justify-center items-center gap-y-2">
             <Text>Criar usuario</Text>
-            <Text>Nome</Text>
-            <Controller 
-                control={control}
-                rules={{
-                    required: "Digite um nome",
-                }}
-                render={({
-                    field: {onChange, onBlur, value}}) => (
-                        <TextInput 
-                            placeholder="Digite seu nome"
-                            onBlur={onBlur}
-                            onChangeText={onChange}
-                            value={value}
-                        />
-                    )}
-                name="Name"
-            />
-            {errors.Name && <Text>{errors.Name.message}</Text>}
+            <View>
+                
+                <Text className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nome</Text>
+                <Controller 
+                    control={control}
+                    rules={{
+                        required: "Digite um nome",
+                    }}
+                    render={({
+                        field: {onChange, onBlur, value}}) => (
+                            <TextInput 
+                                className="py-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder="Digite seu nome"
+                                onBlur={onBlur}
+                                onChangeText={onChange}
+                                value={value}
+                            />
+                        )}
+                    name="Name"
+                />
+                {errors.Name && <Text>{errors.Name.message}</Text>}
 
-            <Text>Email</Text>
-            <Controller 
-                control={control}
-                rules={{
-                    required: "Digite um email",
-                    pattern: {
-                        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                        message: "Digite um endereço de email valido"
-                    }
-                }}
-                render={({
-                    field: {onChange, onBlur, value}}) => (
-                        <TextInput 
-                            placeholder="Digite seu nome"
-                            onBlur={onBlur}
-                            onChangeText={onChange}
-                            value={value}
-                        />
-                    )}
-                name="Email"
-            />
-            {errors.Email && <Text>{errors.Email.message}</Text>}
+                <Text className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</Text>
+                <Controller 
+                    control={control}
+                    rules={{
+                        required: "Digite um email",
+                        pattern: {
+                            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                            message: "Digite um endereço de email valido"
+                        }
+                    }}
+                    render={({
+                        field: {onChange, onBlur, value}}) => (
+                            <TextInput
+                                className="py-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder="Digite seu nome"
+                                onBlur={onBlur}
+                                onChangeText={onChange}
+                                value={value}
+                            />
+                        )}
+                    name="Email"
+                />
+                {errors.Email && <Text>{errors.Email.message}</Text>}
 
-            <Text>Senha</Text>
-            <Controller 
-                control={control}
-                rules={{
-                    required: true,
-                    minLength: 6
-                }}
-                render={({
-                    field: {onChange, onBlur, value}}) => (
-                        <TextInput 
-                            placeholder="Digite sua senha"
-                            onBlur={onBlur}
-                            onChangeText={onChange}
-                            value={value}
-                        />
-                    )}
-                name="Password"
-            />
-            {errors.Password && <Text>{errors.Password.message}</Text>}
+                <Text className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Senha</Text>
+                <Controller 
+                    control={control}
+                    rules={{
+                        required: true,
+                        minLength: 6
+                    }}
+                    render={({
+                        field: {onChange, onBlur, value}}) => (
+                            <TextInput 
+                                className="py-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder="Digite sua senha"
+                                onBlur={onBlur}
+                                onChangeText={onChange}
+                                value={value}
+                            />
+                        )}
+                    name="Password"
+                />
+                {errors.Password && <Text>{errors.Password.message}</Text>}
+            </View>
             
             <Button
                 title="Criar"
