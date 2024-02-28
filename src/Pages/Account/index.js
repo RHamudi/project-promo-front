70a56202177b8e44b6f9b7d71af26bf22e510638
Hamudi <ramonramos.silva16@gmail.com>
2024-02-28 16:04:49 -0300

    feat: adding req get business in page account

--- src/Pages/Account/index.js
+++ src/Pages/Account/index.js
@@ -1,28 +1,63 @@
-import { Button, Text, View } from "react-native";
+import { Button, Image, Text, View } from "react-native";
 import { useSelector } from 'react-redux';
 import { useStateLogin } from '../../Redux/LoginSlice';
+import { useEffect } from "react";
+import { axiosApi } from "../../Services/http-client";
+import { useState } from "react";
 
 export default function Account({navigation}){
     const {Token} = useSelector(useStateLogin)
     const {User} = useSelector(useStateLogin)
+    const [Business, setBusiness] = useState();
+    const [Loading, setLoading] = useState(true);
 
-    
+    useEffect(()=> {
+        axiosApi.get(`business/getbyid?idEmpresa=${User.idEmpresa}`)
+        .then((res)=> {
+            setBusiness(res.data.data) 
+            setLoading(false)
+        })
+        .catch((err)=>console.log(err))
+    },[])
     return (
-        <View>
-            <Text>Usuario: {User.nome}</Text>
-            {User.idEmpresa ? 
-            null
+        <>
+        {Loading ? 
+            (
+                <View> 
+                    <Text>Carregando...</Text>
+                </View>
+            )
             :
-            (<Button 
-                title="Criar Empresa"
-                onPress={()=> navigation.navigate("AddBusiness")}
-            />)
-            }
-            
-            <Button
-                title="Criar produtos"
-                onPress={()=> navigation.navigate("AddProduct")}
-            />
-        </View>
+            (
+                <View>
+                    <View className="flex-col items-center h-20">
+                        <Image 
+                            className="w-40 h-40 rounded-full"
+                            
+                            src={Business.logoImagem}
+                        />
+                        <Text>{Business.nome}</Text>
+                        
+                </View>
+                <Text>Usuario: {User.nome}</Text>
+                {User.idEmpresa ? 
+                null
+                :
+                (<Button 
+                    title="Criar Empresa"
+                    onPress={()=> navigation.navigate("AddBusiness")}
+                />)
+                }
+                
+                <Button
+                    title="Criar produtos"
+                    onPress={()=> navigation.navigate("AddProduct")}
+                />
+            </View>
+            )
+        }
+        
+        </>
+        
     )
 }
