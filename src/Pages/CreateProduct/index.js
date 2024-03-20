2b77c9238c321cbac136f6384d916553ffc55e2b
Hamudi <ramonramos.silva16@gmail.com>
2024-03-20 12:24:50 -0300

    feat: adding styles

--- src/Pages/CreateProduct/index.js
+++ src/Pages/CreateProduct/index.js
@@ -63,80 +63,87 @@ export default function CreateProduct(){
     }
 
     return (
-        <View >
-            <Text>Digite o nome do produto</Text>
-            <Controller 
-                control={control}
-                rules={{
-                    required: true,
-                    pattern: {
-                        message: "Digite um nome valido"
-                    }
-                }}
-                render={({
-                    field: {onChange, onBlur, value}}) => (
-                        <TextInput 
-                            placeholder="Digite o nome do produto"
-                            onBlur={onBlur}
-                            onChangeText={onChange}
-                            value={value}
-                        />
-                    )}
-                name="Name"
-            />
-            {errors.Name && <Text>{errors.Name.message}</Text>}
+        <View className="flex-1 justify-center items-center gap-y-2 bg-blue-950">
+            <View className="justify-center">
+                    <Text className="font-bold text-4xl text-white text-center pb-14">Adicione seu produto</Text>
+                
+                <Text  className="font-bold text-x text-white text-center pb-3"> Digite o nome do produto</Text>
+                <Controller
+                    control={control}
+                    rules={{
+                        required: true,
+                        pattern: {
+                            message: "Digite um nome valido"
+                        }
+                    }}
+                    render={({
+                        field: {onChange, onBlur, value}}) => (
+                            <TextInput 
+                                placeholder="Digite o nome do produto"
+                                className="border-2 border-cyan-600 rounded-2xl p-1.5 text-slate-400 mb-3 px-6"
+                                onBlur={onBlur}
+                                onChangeText={onChange}
+                                value={value}
+                            />
+                        )}
+                    name="Name"
+                />
+                {errors.Name && <Text>{errors.Name.message}</Text>}
 
-            <Text>Digite a descrição do produto</Text>
-            <Controller 
-                control={control}
-                rules={{
-                    required: true,
-                    pattern: {
-                        message: "Digite uma descrição valida"
-                    }
-                }}
-                render={({
-                    field: {onChange, onBlur, value}}) => (
-                        <TextInput 
-                            placeholder="Digite a descrição do produto"
-                            onBlur={onBlur}
-                            onChangeText={onChange}
-                            value={value}
-                        />
-                    )}
-                name="Description"
-            />
-            {errors.Description && <Text>{errors.Description.message}</Text>}
-            <Text>Selecione sua imagem</Text>
-            <Button 
-                title="Selecione a imagem"
-                onPress={pickImage}
-            />
-            <Text>Digite o preço</Text>
-            <Controller 
-                control={control}
-                rules={{
-                    required: true,
-                    pattern: {
-                        message: "Digite um preço"
-                    }
-                }}
-                render={({
-                    field: {onChange, onBlur, value}}) => (
-                        <TextInput 
-                            placeholder="Digite o preco do produto"
-                            onBlur={onBlur}
-                            onChangeText={onChange}
-                            value={value}
-                        />
-                    )}
-                name="Price"
-            />
-            {errors.Price && <Text>{errors.Price.message}</Text>}
-            <Button 
-            title="Submit"
-            onPress={handleSubmit(onSubmit)}
-            />
+                <Text className="font-bold text-x text-white text-center pb-3">Digite a descrição do produto</Text>
+                <Controller 
+                    control={control}
+                    rules={{
+                        required: true,
+                        pattern: {
+                            message: "Digite uma descrição valida"
+                        }
+                    }}
+                    render={({
+                        field: {onChange, onBlur, value}}) => (
+                            <TextInput 
+                                placeholder="Digite a descrição do produto"
+                                className="border-2 border-cyan-600 rounded-2xl p-1.5 text-slate-400 mb-3 px-6"
+                                onBlur={onBlur}
+                                onChangeText={onChange}
+                                value={value}
+                            />
+                        )}
+                    name="Description"
+                />
+                {errors.Description && <Text>{errors.Description.message}</Text>}
+                <Text className="font-bold text-x text-white text-center pb-3">Selecione sua imagem</Text>
+                <Button 
+                    title="Selecione a imagem"
+                    onPress={pickImage}
+                />
+                <Text className="font-bold text-x text-white text-center pb-3">Digite o preço</Text>
+                <Controller 
+                    control={control}
+                    rules={{
+                        required: true,
+                        pattern: {
+                            message: "Digite um preço"
+                        }
+                    }}
+                    render={({
+                        field: {onChange, onBlur, value}}) => (
+                            <TextInput 
+                                placeholder="Digite o preco do produto"
+                                className="border-2 border-cyan-600 rounded-2xl p-1.5 text-slate-400 mb-3 px-6"
+                                onBlur={onBlur}
+                                onChangeText={onChange}
+                                value={value}
+                            />
+                        )}
+                    name="Price"
+                />
+                {errors.Price && <Text>{errors.Price.message}</Text>}
+                <Button 
+                title="Submit"
+                onPress={handleSubmit(onSubmit)}
+                />
+            </View>
         </View>
     )
 }
