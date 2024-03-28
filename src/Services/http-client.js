3532b26b66518be897e8432f143c4cffc387832f
Hamudi <ramonramos.silva16@gmail.com>
2024-03-28 17:43:08 -0300

    fix: api url

--- src/Services/http-client.js
+++ src/Services/http-client.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const requestAPI = "http://3.143.14.147:80/api/"
+const requestAPI = "https://674e-2804-88c-73ce-f600-9d7e-6293-e934-dc7c.ngrok-free.app/api/"
 
 export const axiosApi = axios.create({
     baseURL: requestAPI,
